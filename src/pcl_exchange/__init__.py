450944b0e76e702eef08e6845524fa4ed8ab93f1
gb-murray <gmurra12@jh.edu>
2026-09-03 11:48:06 -0400

    Updated error model to include ack/nack; implemented transport-agnostic callback client
    Addresses #8, #9

--- src/pcl_exchange/__init__.py
+++ src/pcl_exchange/__init__.py
@@ -1,8 +1,10 @@
-from .builder import PCLMessageBuilder
+from .builder import PCLMessageBuilder, build_ack, build_nack
 from .models import PCLEnvelope, PCLActionContent, PCLError, PCLErrorCode
 
 __all__ = [
     "PCLMessageBuilder",
+    "build_ack",
+    "build_nack",
     "PCLEnvelope",
     "PCLActionContent",
     "PCLError",
